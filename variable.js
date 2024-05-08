ec37a6085fb3967520aa99667e70ed1d1221a382
VishnuBhangale <bhangale.vishnu7@gmail.com>
2024-05-08 07:16:46 +0530

    Variable in TypeScript
    Variable in TypeScript

---
+++ variable.js
@@ -0,0 +1,15 @@
+var n1 = 10; //Type Annotation
+console.log(n1);
+var s1 = "you are the creator of ur destiny";
+var s2 = 'powerful people come from powerful places';
+console.log(s1);
+console.log(s2);
+var b1 = true;
+var b2 = false;
+var b3;
+console.log(b1); // true
+console.log(b2); // false
+function test() {
+    b3 = false;
+    console.log(b3); // 
+}
